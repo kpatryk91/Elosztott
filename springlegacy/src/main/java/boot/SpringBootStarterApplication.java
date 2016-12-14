30b67498ebb6277dcc80cf10fd6edd954068326c
Patrik A5USL0 <kpatryk91@gmail.com>
2016-12-14 16:01:15 +0100

    Spring boot application (7)

--- springlegacy/src/main/java/boot/SpringBootStarterApplication.java
+++ springlegacy/src/main/java/boot/SpringBootStarterApplication.java
@@ -1,20 +1,26 @@
 package boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ImportResource;
 
-//@SpringBootConfiguration
-@ImportResource({"sajat-servlet.xml", "sajat-security.xml"})
+@SpringBootConfiguration
+@ImportResource({ "/sajat-servlet.xml", "sajat-security.xml" })
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages = {
+		"hu.meiit" }, excludeFilters = @Filter(type = FilterType.ANNOTATION, value = Configuration.class))
+// ezzel megy egyéblént handerMapping Error
 @EnableAutoConfiguration
+
 public class SpringBootStarterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStarterApplication.class, args);
 	}
-	
+
 }
